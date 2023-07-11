dd960d9b8e290894e7e5f557b79407a2a6c3f20d
Colin Huber <cbhuber@gmail.com>
2023-07-10 22:00:21 -0600

    feat: fetch menu data with loaders.

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import CartOverview from "../features/cart/CartOverview";
 import { Outlet } from "react-router-dom";
+
 function AppLayout() {
   return (
     <div>
