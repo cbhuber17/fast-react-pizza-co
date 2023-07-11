f3bf6a703b2916e232ecbc8d14654adbcd0d51d4
Colin Huber <cbhuber@gmail.com>
2023-07-10 21:01:46 -0600

    env: integrate starter files.

---
+++ src/features/cart/CartItem.jsx
@@ -0,0 +1,19 @@
+import { formatCurrency } from "../../utils/helpers";
+
+/* eslint react/prop-types: 0 */
+function CartItem({ item }) {
+  const { pizzaId, name, quantity, totalPrice } = item;
+
+  return (
+    <li>
+      <p>
+        {quantity}&times; {name}
+      </p>
+      <div>
+        <p>{formatCurrency(totalPrice)}</p>
+      </div>
+    </li>
+  );
+}
+
+export default CartItem;
