47f25fa2e019bb919da4562d92fcf7de0a0579e7
Colin Huber <cbhuber@gmail.com>
2023-07-12 16:32:35 -0600

    feat: modelling user using redux.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import Home from "./ui/Home";
 import Error from "./ui/Error";
 import AppLayout from "./ui/AppLayout";
-import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import CreateOrder, {
   action as createOrderAction,
 } from "./features/order/CreateOrder";
-import Order, { loader as orderLoader } from "./features/order/Order";
 
 const router = createBrowserRouter([
   {
